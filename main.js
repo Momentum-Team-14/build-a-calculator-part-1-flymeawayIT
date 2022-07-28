31ef83cd2e2b4342f625d6561e5a87743cb01fde
Gary Whitford <ssrdunc@gmail.com>
2022-07-28 14:23:00 -0400

    Revisions from Codepen to VSC

--- main.js
+++ main.js
@@ -1,40 +1,51 @@
-// This function clears all the values
-function clear() {
-
-    document.getElementByClass("clear").value= "";
- 
- }
- 
- // This function display values
- 
- function display(equal) {
-     document.getElementByClass("result").value += value;
- 
- }
- 
- // This function evaluates the expression and returns result
- 
- function calculate() {
- 
-     let p = document.getElementByClass("button")          .value;
-     let q = eval(p);
- 
-     document.getElementByClass("button") .value              = q;
- 
- }
- 
- const calculator = {
-   displayValue: '0',
-   firstOperand: null,
-   waitingForSecondOperand: false,
-   operator: null,
- }
- 
- function updateDisplay() {
-   // select the element with class of `calculator-screen`
-   const display = document.querySelector('.calculator-screen');
-   // update the value of the element with the contents of `displayValue`
-   display.value = calculator.displayValue;
- }
- 
- 
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  }
+  
+  
+  document.getElementByClass('button') { button.addEventListener('click', displayNumber)('calculator-screen')
+    
+  })
+  
+  document.getElementByClass('operator')
+  button.addEventListener('click', function {
+                          
+                          
+                          }                       )
+  // This function clears all the values
+  function clear() {
+  
+     document.getElementByClass("clear").value= "";
+  
+  }
+  
+  // This function display values
+  
+  function display(equal) {
+      document.getElementByClass("number").value += value;
+  
+  }
+  
+  // This function evaluates the expression and returns result
+  
+  function calculate() {
+  
+      let p = document.getElementByClass("button")          .value;
+      let q = eval(p);
+  
+      document.getElementByClass("button") .value              = q;
+  
+  }
+  
+  
+  function updateDisplay("calculator-screen") {
+    // select the element with class of `calculator-screen`
+    const display = document.querySelector('number');
+    // update the value of the element with the contents of `displayValue`
+    display.value = calculator.displayValue;
+  }
+  
+  
