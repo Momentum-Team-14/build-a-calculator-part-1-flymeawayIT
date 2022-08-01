e7179c09a87d193e5f9ab9cf1f7cbb7291159c33
Gary Whitford <ssrdunc@gmail.com>
2022-08-01 07:58:47 -0400

    More attempts to correct JS

--- main.js
+++ main.js
@@ -6,48 +6,69 @@ const calculator = {
     operator: null,
   }
   
-  
-  document.getElementsByClass('button') { button.addEventListener('click', displayNumber)('calculator-screen')
-    
-  })
-  
-  document.getElementsByClass('operator')
-  button.addEventListener('click', function {
-                          
-                          
-                          }                       )
-  // This function clears all the values
-  function clear() {
-  
-     document.getElementsByClass("clear").value= "";
-  
+const calculator = document.querySelector('.calculator');
+const numberKey = document.querySelector('.button');
+const operatorKey = document.querySelector('.operator');
+const display = document.querySelector('result-screen');
+const clear = document.querySelector('clear');
+const equal = document.querySelector"('equal');
+
+
+const key = event.target
+const keyValue = key.textContent
+const displayValue =display.textContent
+const { type } = key.dataset
+const { previousKeyType } = calculator.dataset
+
+if (type === 'number') {
+  if (displayValue === "0" || previousKeyType === 'operator') { display.textContent = keyValue }
+  else { display.textContent = displayValue + keyValue
+} 
+}
+
+
+if (type === 'operator') { const operatorKeys = keys.querySelectorAll('[button="operator"]') operatorKey.forEach(el => { el.dataset.state = ''}) key.dataset.state = ()'selected')
+                         
+  calculator.dataset.firstNumber = dispalyValue
+  calculator.dataset.opeartor = key.dataset.key                        
   }
+
+if (type === 'equal') { 
+  const firstNumber =                calculator.dataset.firstNumber
+  const operator - calculator.dataset.operator
+  const secondNumber = displayValue
+  display.textContent = calculate(firstNumber, operator, secondNumber)
+  comnsole.log(display.textContent)
   
-  // This function display values
+}
+
+if (type === 'clear') {
+display.textContent = '0'
+  delete calculator.dataset.firstNumber
+  delete calculator.dataset.operator
+}
+
+calculator.dataset.previousKeyType = type
+}
+
+function calculate (firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber)
+  secondNumber = parseInt(secondNumber)
   
-  function display(equal) {
-      document.getElementsByClass("number").value += value;
+  if (operator === '+') return firstNumber + secondNumber
+  if (operator === '-') return firstNumber - secondNumber
+  if (operator === '*') return firstNumber * secondNumber
+  if (operator === '/') return firstNumber /  secondNumber
   
-  }
-  
-  // This function evaluates the expression and returns result
-  
-  function calculate() {
-  
-      let p = document.getElementByClass("button")          .value;
-      let q = eval(p);
-  
-      document.getElementByClass("button") .value              = q;
-  
-  }
-  
-  
-  function updateDisplay("result-screen") {
-    // select the element with class of `calculator-screen`
-    const display = document.querySelector('number');
-    // update the value of the element with the contents of `displayValue`
-    display.value = calculator.displayValue;
-  }
-  
-  const clickSoundMain = document.getElementById('#click-button')
-  const clickSoundMain = document.getElementById('#click-operator')
+}
+ 
+
+function updateDisplay("result-screen") {
+  // select the element with class of `calculator-screen`
+  const display = document.querySelector('number');
+  // update the value of the element with the contents of `displayValue`
+  display.value = calculator.displayValue;
+}
+
+const clickSoundMain = document.getElementById('#click-button')
+const clickSoundMain = document.getElementById('#click-operator')
